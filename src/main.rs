88816a1d50247e85f594881a0b77469f746ddb1a
Andre Ambrosio Boechat <andre.boechat@belvo.com>
2022-08-09 21:12:42 -0300

    Add code to read seeds and print the grid
    commit e15ee79c29dadd4993d28a820e3dcca5770b1032
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Sun Aug 7 21:36:43 2022 -0300
    
        Make the seed parser easier to test
    
    commit 03bf5e5e3c84e34b86dcbdf25ef55ee2e9a17104
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Sun Aug 7 21:27:19 2022 -0300
    
        Print the generation number
    
    commit 20ab6e03382f6a17b29e8d21ba420e4495f81eba
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Sun Aug 7 21:24:01 2022 -0300
    
        Add an empty function to update generations and use it in the main loop
    
    commit 0fcd61185f086fd5ab7f612c8bdb809daef2ff92
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Sun Aug 7 21:01:12 2022 -0300
    
        Read and parse seed configuration from STDIN
    
    commit bd0d2b21e8ce4282b34b1ed758f49f724ea56990
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Sun Aug 7 13:31:40 2022 -0300
    
        Keep the idle cursor at the correct place (at bottom)
    
    commit 06b745196bd4ae50a8cd6efb0511abd431d81f9e
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Sun Aug 7 13:24:31 2022 -0300
    
        Print two different grids in an infinite loop

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,68 @@
+use std::{io, io::Read, io::Write, thread, time::Duration};
+
 type Cell = bool;
 type Grid = Vec<Vec<Cell>>;
 
-fn repr_cell(cell: Cell) -> &'static str {
-    if cell {
-        "■ "
-    } else {
-        ". "
-    }
-}
-
-fn print_grid(grid: Grid) {
-    for row in &grid {
-        println!("");
+/// Prints a grid of cells on the screen.
+fn print_grid(grid: &Grid) {
+    for row in grid {
         for cell in row {
-            print!("{} ", repr_cell(*cell));
+            print!("{} ", if *cell { "■ " } else { ". " });
         }
+        println!();
     }
 }
 
-fn main() {
-    print_grid(vec![vec![true, false], vec![false, true]]);
+/// Parses a seed text and returns a Grid.
+///
+/// The seed is a multi-line text with 0s and 1s.
+///
+/// ```
+/// 0 0 0
+/// 0 1 0
+/// 0 0 0
+/// ```
+fn text_to_grid(text: &str) -> Grid {
+    fn line_to_cells(line: &str) -> Vec<Cell> {
+        line.split_whitespace()
+            .map(|word| word.chars().next() == Some('1'))
+            .collect()
+    }
+    text.lines().map(line_to_cells).collect()
+}
+
+/// Returns a Grid from a seed given by STDIN.
+fn stdin_to_grid() -> io::Result<Grid> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    Ok(text_to_grid(&buffer))
+}
+
+fn update_generation(grid: &mut Grid) {
+    grid;
+    // TODO
+}
+
+fn main() -> io::Result<()> {
+    // We expect the initial grid from STDIN.
+    let mut grid = stdin_to_grid()?;
+    // This clears the shell.
+    print!("{esc}[2J", esc = 27 as char);
+    // We iterate forever. We should stop the program with "Ctrl-c".
+    for i in 0..5 {
+        // We move the cursor to "Home" again to print over the previous
+        // characters.
+        print!("{esc}[H", esc = 27 as char);
+        println!("Generation {}", i);
+        print_grid(&grid);
+        // "print!" is buffered. We should flush it to get the visual effect we
+        // want.
+        io::stdout().flush().unwrap();
+        // Sleeping between generations.
+        thread::sleep(Duration::from_millis(700));
+        // We update the current generation of cells following the game rules.
+        update_generation(&mut grid);
+    }
+
+    Ok(())
 }
