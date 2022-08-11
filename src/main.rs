f832b7f202b9d179bf67980953a1afa54ad21173
Andre Ambrosio Boechat <andre.boechat@belvo.com>
2022-08-11 20:37:39 -0300

    Add the first implementation of the game's algorithm
    commit 1deb4638e4db408addae30394285695b0c83b12f
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Thu Aug 11 20:36:17 2022 -0300
    
        Improve readability
    
    commit b6d53a9ade9a1ee0745a0466cfea1231093f7272
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Thu Aug 11 20:33:30 2022 -0300
    
        Remove extra black space in cells
    
    commit 8f92a1b52ea4de50d763a42c8e7e33f13186b614
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Thu Aug 11 20:21:04 2022 -0300
    
        Improve readability
    
    commit ecbadb0e84cc42e40576e61cad70aabcb3640fd9
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Wed Aug 10 21:38:56 2022 -0300
    
        Abstract the calculation of minimum and maximum 8-neighborhood indexes
    
    commit d34840825dd95d5545b1f87228a205d019d35342
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Wed Aug 10 16:46:52 2022 -0300
    
        Improve readability
    
    commit 38f68569013af7ca976a48290e66d841a2898e33
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Wed Aug 10 16:33:33 2022 -0300
    
        Make the code correct (still ugly)
    
    commit def092d8b3b5b2fb438a73da5d879d9edc673bc8
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Wed Aug 10 15:33:37 2022 -0300
    
        Add first ugly and wrong implementation
    
    commit e3e1737d7eff847d4aec70265b973c00c84eb9b3
    Author: Andre Ambrosio Boechat <andre.boechat@belvo.com>
    Date:   Wed Aug 10 08:28:19 2022 -0300
    
        Use 0s and 1s to represent cells (they are easier to calculate the neighbors)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::{io, io::Read, io::Write, thread, time::Duration};
 
-type Cell = bool;
+type Cell = u8;
 type Grid = Vec<Vec<Cell>>;
 
 /// Prints a grid of cells on the screen.
 fn print_grid(grid: &Grid) {
     for row in grid {
         for cell in row {
-            print!("{} ", if *cell { "■ " } else { ". " });
+            print!("{} ", if *cell == 0 { "." } else { "■" });
         }
         println!();
     }
@@ -25,7 +25,7 @@ fn print_grid(grid: &Grid) {
 fn text_to_grid(text: &str) -> Grid {
     fn line_to_cells(line: &str) -> Vec<Cell> {
         line.split_whitespace()
-            .map(|word| word.chars().next() == Some('1'))
+            .map(|word| word.parse::<u8>().unwrap())
             .collect()
     }
     text.lines().map(line_to_cells).collect()
@@ -38,9 +38,46 @@ fn stdin_to_grid() -> io::Result<Grid> {
     Ok(text_to_grid(&buffer))
 }
 
-fn update_generation(grid: &mut Grid) {
-    grid;
-    // TODO
+fn sum_neighbors(grid: &Grid, i: usize, j: usize) -> u8 {
+    // These functions help us find the minimum and maximum index values in the
+    // 8-neighborhood.
+    let imin0 = |x| if x > 0 { x - 1 } else { 0 };
+    let imax = |x, y| if x < y { x + 1 } else { y };
+    // These are the minimum and maximum indexes in the 8-neighborhood.
+    // Row indexes.
+    let min_r_idx = imin0(i);
+    let max_r_idx = imax(i, grid.len() - 1);
+    // Column indexes.
+    let min_c_idx = imin0(j);
+    let max_c_idx = imax(j, grid[0].len() - 1);
+    // Summing up the values of all neighbors of the cell (i, j).
+    (min_r_idx..=max_r_idx).fold(0, |acc, ii| {
+        // Iterating on row indexes.
+        acc + (min_c_idx..=max_c_idx).fold(0, |acc, jj| {
+            // Iterating on column indexes.
+            acc + grid[ii][jj]
+        })
+        // We want only the sum of the neighbors of the cell (i, j); we
+        // need to subtract the value of the cell.
+    }) - grid[i][j]
+}
+
+/// Returns "true" if "cell" is alive when considering the number of live
+/// neighbors "n_neig".
+fn is_alive(cell: u8, n_neig: u8) -> bool {
+    (cell > 0 && 1 < n_neig && n_neig < 4) || (cell == 0 && n_neig == 3)
+}
+
+fn next_generation(grid: &Grid) -> Grid {
+    let mut sumgrid = vec![vec![0 as u8; grid[0].len()]; grid.len()];
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            let cell = grid[i][j];
+            let sum = sum_neighbors(grid, i, j);
+            sumgrid[i][j] = if is_alive(cell, sum) { 1 } else { 0 }
+        }
+    }
+    sumgrid
 }
 
 fn main() -> io::Result<()> {
@@ -49,7 +86,7 @@ fn main() -> io::Result<()> {
     // This clears the shell.
     print!("{esc}[2J", esc = 27 as char);
     // We iterate forever. We should stop the program with "Ctrl-c".
-    for i in 0..5 {
+    for i in 0.. {
         // We move the cursor to "Home" again to print over the previous
         // characters.
         print!("{esc}[H", esc = 27 as char);
@@ -60,9 +97,23 @@ fn main() -> io::Result<()> {
         io::stdout().flush().unwrap();
         // Sleeping between generations.
         thread::sleep(Duration::from_millis(700));
-        // We update the current generation of cells following the game rules.
-        update_generation(&mut grid);
+        // We calculate the next generation of cells following the game rules.
+        grid = next_generation(&grid);
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod main_test {
+    use super::{next_generation, text_to_grid};
+
+    #[test]
+    fn test_next_generation() {
+        let prev_gen = "0 1 0 0\n\
+                        1 0 0 0\n\
+                        0 1 0 0";
+        let next_gen = next_generation(&text_to_grid(prev_gen));
+        assert_eq!(next_gen, [[0, 0, 0, 0], [1, 1, 0, 0], [0, 0, 0, 0]]);
+    }
 }
